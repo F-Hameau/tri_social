8b718f69db7cb3830a6926a387ab2aa02657e43b
François HAMEAU <f.hameau@gmail.com>
2019-01-21 17:34:32 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
 
-  validates :name, presence: { message: 'Ce champ doit être complété' }
+  validates :name, presence: true
   validates :name, uniqueness: { message: 'Ce nom est déjà pris' }
   validates :password, format: { with: /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/, message: 'Le mot de passe doit contenir au moins 1 chiffre et 1 lettre et être composé de 8 caractères' }
   validates :email, format: { with: /\A.*@.*\..*\z/, message: "L'email est invalide" }
